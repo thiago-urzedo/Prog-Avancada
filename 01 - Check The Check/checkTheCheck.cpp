76b588bd94badf84c3cd5e96e294ba0890e61862
Thiago Urzedo <tusp11@gmail.com>
2025-06-24 14:18:34 -0300

    Finalizado exercicio 07 e avancando com o 01

--- 01 - Check The Check/checkTheCheck.cpp
+++ 01 - Check The Check/checkTheCheck.cpp
@@ -1,10 +1,42 @@
 #include <iostream>
+#include <fstream>
+#include <vector>
 
 using namespace std;
 
 int main() {
 
+    char square;
+    ifstream input;
     
+    input.open("input.txt");
+    if (!input.is_open()) {
+        cerr << "Erro ao abrir o arquivo" << endl;
+        return 1;
+    }
+    
+    bool exit = false;
+    while (!exit) {
+        vector<vector<char>> board(8, vector<char>(8));
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 8; j++) {
+                input.get(square);
+                if (square == '\n' || square == ' ') {
+                    i--;
+                    continue;
+                }
+                cout << square << " ";
+            }
+            cout << endl;
+            input.get(square);
+            if (input.eof()) {
+                exit = true;
+                break;
+            }
+        }
+    }
+    
+    input.close();
 
     return 0;
 }
